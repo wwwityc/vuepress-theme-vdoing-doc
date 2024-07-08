1a739c5705bdf1dba24e6e0e78df21c4ece3b1de
wangtiankong <305038732@qq.com>
2024-07-08 11:41:36 +0800

    f

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -57,10 +57,6 @@ module.exports = {
           { text: '评论栏', link: '/pages/ce175c/' },
         ]
       },
-      { text: '资源', link: '/pages/db78e2/' },
-      { text: '案例', link: '/pages/5d571c/' },
-      { text: '问答', link: '/pages/9cc27d/' },
-      { text: '赞助', link: '/pages/1b12ed/' },
     ],
     sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
     logo: '/img/logo.png', // 导航栏logo
